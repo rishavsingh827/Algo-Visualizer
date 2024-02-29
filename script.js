badb6523262905ad6d0ee0e36263a36179ba96a5
rishavsingh827 <92147422+rishavsingh827@users.noreply.github.com>
2024-02-29 22:38:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+myCanvas.width = 500;
+myCanvas.height = 400;
+const margin = 30;
+const n = 40;
+const array = [];
+let moves = [];
+const cols = [];
+const spacing = (myCanvas.width - margin * 2) / n;
+const ctx = myCanvas.getContext("2d");
+
+const maxColumnHeight = 200;
+
+
+init();
+let audioCtx = null;
+
+
+
+
+function init() {
+    for (let i = 0; i < n; i++) {
+        array[i] = Math.random();
+    }
+    moves = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const get3d = i * 2;
+        const x = i * spacing + spacing / 2 + margin;
+        const y = myCanvas.height - margin - get3d;
+        const width = spacing - 3; //to get margin we subtracted - 4
+        const height = maxColumnHeight * array[i];
+
+        cols[i] = new Column(x, y, width, height);
+
+    }
+}
+function play() {
+    moves = bubbleSort(array);
+}
+
+
+
+
+
+animate();
+
+function bubbleSort(array) {
+    const moves = [];
+    do {
+        var swapped = false;
+        for (let i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                swapped = true;
+                [array[i - 1], array[i]] = [array[i], array[i - 1]];
+                moves.push(
+                    { indices: [i - 1, i], swap: true }
+                );
+            }
+            else {
+                moves.push(
+                    { indices: [i - 1, i], swap: false }
+                )
+            }
+        }
+    } while (swapped);
+    return moves;
+
+}
+function animate() {
+
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    let changed = false;
+    for (let i = 0; i < cols.length; i++) {
+        changed = cols[i].draw(ctx) || changed;
+    }
+    if (!changed && moves.length > 0) {
+        const move = moves.shift();
+        const [i, j] = move.indices;
+        if (move.swap) {
+            cols[i].moveTo(cols[j]);
+            cols[j].moveTo(cols[i], -1);
+            [cols[i], cols[j]] = [cols[j], cols[i]];
+        }
+        else {
+            cols[i].jump();
+            cols[j].jump();
+        }
+    }
+    requestAnimationFrame(animate);
+}
